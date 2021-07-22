845b550e8dafb06888916fe03d492a3804aede32
mrogulji <matej.roguljic@cern.ch>
2021-07-22 08:42:22 +0200

    update datacards

--- saveFit.py
+++ saveFit.py
@@ -8,16 +8,22 @@ def executeCMD(command):
 
 
 fitDir = sys.argv[1]
-fitName = fitDir.split("/")[-1]
+#fitName = fitDir.split("/")[-1]
+fitName = sys.argv[2]
+
+if("NAL" in fitDir):
+	cp_had_workspaces = "cp {0}/NAL*/base*root {1}/hadronic_workspaces/.".format(fitDir,fitName)
+else:
+	cp_had_workspaces = "cp {0}/??/base*root {1}/hadronic_workspaces/.".format(fitDir,fitName)#LL/TT
 
 mkdir 		= "mkdir -p {0}/hadronic_workspaces".format(fitName)
-#cp_CR_cards = "cp {0}/CR_*txt CR_cards/.".format(fitDir,fitName)
-cp_had_workspaces = "cp {0}/NAL*/base*root {1}/hadronic_workspaces/.".format(fitDir,fitName)
+cp_CR_cards = "cp {0}/CR_*txt CR_cards/.".format(fitDir)
 cp_card 		  = "cp {0}/combinedCard.txt {1}/card_{1}.txt".format(fitDir,fitName)
 backup_card 	  = "cp {0}/card_{0}.txt {0}/card_{0}_backup.txt".format(fitName)
 	
 executeCMD(mkdir)
 executeCMD(cp_had_workspaces)
+executeCMD(cp_CR_cards)
 executeCMD(cp_card)
 executeCMD(backup_card)
 
@@ -27,7 +33,11 @@ with open('{0}/card_{0}.txt'.format(fitName), 'r') as file :
 
 # Replace the target string
 filedata = filedata.replace('/afs/cern.ch/work/m/mrogulji/UL_X_YH/X_YH_4b/results/templates_semileptonic/combined/', '../CR_templates/')
-filedata = re.sub("\sNAL_./base"," hadronic_workspaces/base",filedata)
+if("NAL" in fitDir):
+	filedata = re.sub("\sNAL_./base"," hadronic_workspaces/base",filedata)
+else:
+	filedata = re.sub("\sLL/base"," hadronic_workspaces/base",filedata)
+	filedata = re.sub("\sLTT/base"," hadronic_workspaces/base",filedata)
 # Write the file out again
 with open('{0}/card_{0}.txt'.format(fitName), 'w') as file:
   file.write(filedata)
